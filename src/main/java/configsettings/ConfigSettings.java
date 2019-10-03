9aa7f5926d150644ec3b5e7d04d676a96853d48e
Zac Kaiser <kaiserzd4335@uwec.edu>
2019-10-03 16:02:18 -0500

    Added config file reader class and config file

---
+++ src/main/java/configsettings/ConfigSettings.java
@@ -0,0 +1,28 @@
+import java.io.FileReader;
+import java.util.Properties;
+
+public class ConfigSettings {
+    private Properties settings = new Properties();
+
+    public ConfigSettings(String fileName) {
+        try {
+            settings.load(new FileReader(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public String getSetting(String key) {
+        return getSetting(key, null);
+    }
+
+    public String getSetting(String key, String defaultValue) {
+        return settings.getProperty(key, defaultValue);
+    }
+
+    public static void main(String[] args) {
+        ConfigSettings test = new ConfigSettings("settings.cfg");
+        System.out.println(test.getSetting("CHECK_COMMENT_PER_MODULE"));
+    }
+}
