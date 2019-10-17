c50bdab8f7fca5a83efa1e72d313313039f66dad
emadreist <dreistet8917@uwec.edu>
2019-10-17 16:32:31 -0500

    created execution "runprogram" class that runs python script. Does not fully run yet, hanging waiting for piped answers

--- src/main/java/runner/runProgram.java
+++ src/main/java/runner/runProgram.java
@@ -1,15 +1,27 @@
 package runner;
 
 import java.io.*;
+import java.util.Scanner;
 
 
 public class runProgram {
     public static void main(String args[]) throws IOException {
-        String str = null;
-        Process process = Runtime.getRuntime().exec("python python_labs/chual2242_lab04.py");
+        Scanner scanner = new Scanner(System.in);
+        String str;
+        File file = new File("./compare.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        //System.out.println("Please enter text file with user input for program");
+        //String txtFile = scanner.next();
+        //if(txtFile == null) {
+        //    System.out.println("Please enter a valid text file");
+        //}
+        Process process = Runtime.getRuntime().exec("python python_labs/chual2242_lab04_removeDuplicates.py" );
         BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
         while((str = in.readLine()) != null) {
             System.out.println(str);
+            String fileContent = str;
+            writer.write(fileContent);
         }
+        writer.close();
     }
 }
