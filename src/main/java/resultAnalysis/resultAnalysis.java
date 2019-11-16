ac216f658bf893436b251e72efc65f926108a624
00Zacho <z.pothen@gmail.com>
2019-11-16 17:02:11 -0600

    Running analysis on any given text output files

--- src/main/java/resultAnalysis/resultAnalysis.java
+++ src/main/java/resultAnalysis/resultAnalysis.java
@@ -1,6 +1,7 @@
 package resultAnalysis;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,11 +9,12 @@ import java.util.Collections;
 
 public class resultAnalysis {
 
-    private static List result;
+    private static List<Integer> result;
     private static int amountCalled = 0;
     private static List<String> files;
+    private static int numQ;
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         //create list of all the files to analyze
         files = new ArrayList<>();
@@ -22,58 +24,84 @@ public class resultAnalysis {
         files.add(pathTo + "test3.txt");
         files.add(pathTo + "test4.txt");
 
-        //create list to store the amount of wrong answers per question
-        result = new ArrayList<Integer>(Collections.nCopies(4, 0));
+        try {
 
-        //run through all files analyzing each one and putting resulting into the result list
-        for(int i = 0; i < files.size(); i++){
-            try {
-                arrayitize(files.get(i));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //print out the results
-        displayResults(result);
-
-    }
-
-    public static void arrayitize(String path) throws FileNotFoundException {
-        //increment used to find class total aka amount of assignments graded
-        amountCalled++;
-        Scanner in = new Scanner(new File(path));
-        in.nextLine();
-        while (in.hasNextLine()) {
-            String line = in.nextLine().trim();
-            String[] separated = line.split("\t");
-            //Split each line into the following
-            //0: number  1: student  2: instructor   3: answer
-            int question = Integer.parseInt(separated[0].substring(0,1));
-            //if wrong add to that questions total
-            if(separated[3].equals("Wrong")) {
-                result.set(question, (int) result.get(question)+1);
-            }
-
-        }
-    }
-
-
-    public static void displayResults(List results) {
-
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream("filename.txt"), "utf-8"))) {
-            for (int i = 0; i < results.size(); i++) {
-                writer.write("Amount wrong for question " + i + ": " + result.get(i) + " ==> " + ((int) result.get(i) * 100) / amountCalled + "%" + "\n");
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("filename.txt"), StandardCharsets.UTF_8));
+            runAnalysis(files, writer);
+            displayResults(result, writer);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    private static void runAnalysis(List<String> list, Writer writer) throws IOException {
+        //create list to store the amount of wrong answers per question
+        numQ = numQuestions(new File(list.get(0)));
+        result = new ArrayList<>(Collections.nCopies(numQ, 0));
+
+        writer.write("INDIVIDUAL SCORES\n");
+        //run through all files analyzing each one and putting resulting into the result list
+        for (String s : list) {
+            try {
+                splitResult(s, writer);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        writer.write("\n");
+
+    }
+
+    private static void splitResult(String path, Writer writer) throws IOException {
+        //increment used to find class total aka amount of assignments graded
+        amountCalled++;
+        int amountWrong = 0;
+        Scanner in = new Scanner(new File(path));
+        in.nextLine();
+        while (in.hasNextLine()) {
+            String line = in.nextLine().trim();
+            String[] separated = line.split("\t\t\t");
+            //Split each line into the following
+            //0: number  1: student  2: instructor   3: answer
+            int question = Integer.parseInt(separated[0].substring(0,1));
+            //if wrong add to that questions total
+            if(separated[3].equals("Wrong")) {
+                amountWrong++;
+                result.set(question, result.get(question) +1);
+            }
+        }
+        String name = "STUDENT";
+        String grade = getGrade(amountWrong);
+
+        writer.write(name + ": " + grade + "\n");
+
+    }
+
+
+    private static String getGrade(int amountWrong){
+        return (numQ - amountWrong) + "/" + numQ;
+    }
+
+    private static void displayResults(List<Integer> results, Writer writer) throws IOException {
+
+            writer.write("CLASS RESULTS\n");
+            for (int i = 0; i < results.size(); i++) {
+                writer.write( result.get(i)  + " students got question " + i + " wrong. That is " + (result.get(i) * 100) / amountCalled + "% of the class\n");
+            }
+
+    }
+
+    private static int numQuestions(File file) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+        lines -= 1;
+        return lines;
     }
 }
